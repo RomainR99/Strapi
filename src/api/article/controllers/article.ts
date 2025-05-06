1a132245903435dad00e6105d1949f1f6484a892
Romain <romain.rothn@gmail.com>
2025-05-06 12:32:59 +0200

    controller bouton supprimer ok back

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -1,21 +1,36 @@
-/**
- * article controller
- */
-
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::article.article', ({ strapi }) => ({
   async create(ctx) {
     const user = ctx.state.user;
+    console.log('hello');
 
-    // if (!user) {
-    //   return ctx.unauthorized("Vous devez être connecté pour créer un article.");
-    // } pas besoin car le user à un controlleur sur trapi sur sa route avant d'arriver à cette étape
-    console.log('hello')
+    ctx.request.body.data.user = user.id;
 
-    ctx.request.body.data.user = user.id
+    const response = await strapi.controller('api::article.article').create(ctx, async () => {});
+    return response;
+  },
 
+  async delete(ctx) {
+    const user = ctx.state.user;
+    const { id } = ctx.params;
 
-    return await super.create(ctx)
-  }
+    const post = await strapi.entityService.findOne('api::article.article', id, {
+      populate: ['user'],
+    }) as { user?: { id: number } };
+
+    if (!post || !post.user) {
+      return ctx.notFound("Article ou auteur introuvable.");
+    }
+
+    if (post.user.id !== user.id) {
+      return ctx.forbidden("Vous ne pouvez supprimer que vos propres articles.");
+    }
+
+    const response = await strapi.controller('api::article.article').delete(ctx, async () => {});
+    return response;
+  },
 }));
+
+
+
