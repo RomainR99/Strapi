befd7c3adb2703abf7036d86350f906111611d48
Romain <romain.rothn@gmail.com>
2025-05-06 15:26:42 +0200

    suppression  post ok back

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -16,11 +16,26 @@ export default factories.createCoreController('api::article.article', ({ strapi 
 
       // Appel de la méthode de base via super
       return await super.create(ctx);
-    }
+    },
+
+    async delete(ctx) {
+        const user = ctx.state.user;
+        const { id } = ctx.params;
+      
+        const post = await strapi.entityService.findOne('api::article.article', id, {
+          populate: ['user'],
+        }) as { user?: { id: number } };
+      
+        if (!post || !post.user || post.user.id !== user.id) {
+          return ctx.forbidden("Vous ne pouvez supprimer que vos propres articles.");
+        }
+      
+        // Suppression réelle depuis la base de données (Neon)
+        await strapi.entityService.delete('api::article.article', id);
+      
+        return ctx.send({ message: "Article supprimé de la base de données." });
+      }
+      
   };
 });
-
-
-
-
 
