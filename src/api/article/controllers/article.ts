7ec960fbb4f6d5bec1400941ba52ce5e24701935
Romain <romain.rothn@gmail.com>
2025-05-06 14:32:50 +0200

    mise à jour pour delete avec le bouton 6mai 14h32

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -1,36 +1,26 @@
+/**
+ * article controller
+ */
+
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::article.article', ({ strapi }) => ({
-  async create(ctx) {
-    const user = ctx.state.user;
-    console.log('hello');
+export default factories.createCoreController('api::article.article', ({ strapi }) => {
+  return {
+    async create(ctx) {
+      const user = ctx.state.user;
 
-    ctx.request.body.data.user = user.id;
+      // Si on est ici, le user est déjà authentifié (pas besoin de vérification)
+      console.log('hello');
 
-    const response = await strapi.controller('api::article.article').create(ctx, async () => {});
-    return response;
-  },
+      ctx.request.body.data.user = user.id;
 
-  async delete(ctx) {
-    const user = ctx.state.user;
-    const { id } = ctx.params;
-
-    const post = await strapi.entityService.findOne('api::article.article', id, {
-      populate: ['user'],
-    }) as { user?: { id: number } };
-
-    if (!post || !post.user) {
-      return ctx.notFound("Article ou auteur introuvable.");
+      // Appel de la méthode de base via super
+      return await super.create(ctx);
     }
+  };
+});
 
-    if (post.user.id !== user.id) {
-      return ctx.forbidden("Vous ne pouvez supprimer que vos propres articles.");
-    }
 
-    const response = await strapi.controller('api::article.article').delete(ctx, async () => {});
-    return response;
-  },
-}));
 
 
 
