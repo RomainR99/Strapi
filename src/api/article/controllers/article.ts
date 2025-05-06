94d94aa5034328d132e0d7740488ab66cc93be7e
Romain <romain.rothn@gmail.com>
2025-05-06 10:59:13 +0200

    clean code 10H58 pour ajouster post

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -8,25 +8,14 @@ export default factories.createCoreController('api::article.article', ({ strapi 
   async create(ctx) {
     const user = ctx.state.user;
 
-    if (!user) {
-      return ctx.unauthorized("Vous devez être connecté pour créer un article.");
-    }
+    // if (!user) {
+    //   return ctx.unauthorized("Vous devez être connecté pour créer un article.");
+    // } pas besoin car le user à un controlleur sur trapi sur sa route avant d'arriver à cette étape
+    console.log('hello')
 
-    const { data } = ctx.request.body;
+    ctx.request.body.data.user = user.id
 
-    try {
-      const response = await strapi.entityService.create('api::article.article', {
-        data: {
-          ...data,
-          user: user.id, // Associe automatiquement l'utilisateur connecté
-        },
-        populate: { user: true }, // Optionnel : inclure les infos user dans la réponse Pour le token
-      });
 
-      return ctx.send(response);
-    } catch (err) {
-      console.error("Erreur lors de la création d'un article :", err);
-      return ctx.badRequest("Impossible de créer l'article.");
-    }
+    return await super.create(ctx)
   }
 }));
