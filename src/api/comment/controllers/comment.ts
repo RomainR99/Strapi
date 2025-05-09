2e7895d36494018a92e9f7ab575ae929b3adc772
Romain <romain.rothn@gmail.com>
2025-05-09 17:53:34 +0200

    mise à jour controller de comments

--- src/api/comment/controllers/comment.ts
+++ src/api/comment/controllers/comment.ts
@@ -1,15 +1,18 @@
-/**
- * comment controller
- */
+const { createCoreController } = require('@strapi/strapi').factories;
 
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::comment.comment', ({ strapi }) => ({
+module.exports = createCoreController('api::comment.comment', ({ strapi }) => ({
   async create(ctx) {
     try {
+      // Vérification si l'utilisateur est authentifié
       const user = ctx.state.user;
+      if (!user) {
+        return ctx.unauthorized('Vous devez être connecté pour ajouter un commentaire.');
+      }
+
+      // Ajouter l'ID de l'utilisateur dans la requête
       ctx.request.body.data.user = user.id;
 
+      // Appel à la méthode `create` parente pour créer un commentaire
       const response = await super.create(ctx);
       return response;
     } catch (err) {
@@ -19,22 +22,29 @@ export default factories.createCoreController('api::comment.comment', ({ strapi 
 
   async delete(ctx) {
     try {
+      // Récupérer l'utilisateur connecté
       const user = ctx.state.user;
+      if (!user) {
+        return ctx.unauthorized('Vous devez être connecté pour supprimer un commentaire.');
+      }
+
       const { id } = ctx.params;
 
+      // Récupérer le commentaire par ID
       const comment = await strapi.entityService.findOne('api::comment.comment', id, {
-        populate: ['user'],
-      }) as { user?: { id: number } };
-      
+        populate: ['user'], // On s'assure de peupler l'utilisateur associé
+      });
 
       if (!comment) {
         return ctx.notFound('Commentaire non trouvé.');
       }
 
-      if (!comment.user || comment.user.id !== user.id) {
+      // Vérifier si l'utilisateur est l'auteur du commentaire
+      if (comment.user.id !== user.id) {
         return ctx.forbidden('Vous ne pouvez supprimer que vos propres commentaires.');
       }
 
+      // Supprimer le commentaire
       await strapi.entityService.delete('api::comment.comment', id);
       return ctx.send({ message: 'Commentaire supprimé avec succès.' });
     } catch (err) {
@@ -42,4 +52,5 @@ export default factories.createCoreController('api::comment.comment', ({ strapi 
     }
   },
 }));
+
 
